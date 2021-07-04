1234dced94f9be0c225d2e091492b5941a5341a6
Robert <endriosrobert@gmail.com>
2021-07-03 20:11:51 -0500

    primeiro commit

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$dbname = "ead_marinho";
+	
+	//Criar a conexao
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
