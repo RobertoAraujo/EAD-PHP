f8678a01789697a1d93ece98fe650b29c3854aba
Roberto Moraes <robertomoraes@lampp-it.com.br>
2021-09-02 15:18:53 -0500

    Concerto da tela de login

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();	
+	session_start();
 ?>
 <!doctype html>
 <html>
@@ -11,46 +11,48 @@
 		<link rel="stylesheet" type="text/css" href="css/auxiliar.css">
 		<link rel="stylesheet" type="text/css" href="css/grade.css">
 		<link rel="stylesheet" type="text/css" href="css/m-style.css">
-		 
+
 		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 		<script type="text/javascript" src="js/js.js"></script>
-		
+
 	</head>
-<body class="base-login">
-	<div class="caixa-login position-relative">
-		<img src="img/img-topo-login.png">
-			<form method="POST" action="valida.php">						
-					<h1>login </h1>	
+<body class="base-login" style="background-image: url(img/bag.png);">
+	<div class="caixa-login position-relative" >
+		<!-- <img src="img/img-topo-login.png/"> -->
+			<form method="POST" action="valida.php">
+					<h1 style="font-family:Impact, fantasy; color:#e63415;"> ACESSO - EAD </h1>
 					<?php
 						if(isset($_SESSION['msg'])){
 							echo $_SESSION['msg'];
 							unset($_SESSION['msg']);
 						}
-					?>		
+					?>
 					<label>
 						<input type="text" name="usuario" value="" placeholder="Login">
-					</label>				
+					</label>
 					<label>
 						<input type="password" name="senha" value="" placeholder="Senha">
-					</label>							
+					</label>
 					<input type="submit" name="btnLogin" value="Entrar" class="btn">
 			</form>
-		<a href="" class="senha text-azul mt-3 d-block">Esqueci minha senha</a>
-	
+
+
+
 	<div class=" mostrasenha">
 		<div class="caixa">
-			<span class="sair senha">X</span>
-			<h1 class="h3 mb-0 pb-1">Esqueci minha senha </h1>
-			<p class="text-center pb-4">Digite seu email no campo abaixo para recuperar sua senha</p>
+			<span class="sair senha">[ X ]</span>
+			<h1 class="h3 mb-0 pb-1" style="font-family:Impact, fantasy; color:#e63415;">Esqueci minha senha </h1>
+			<p class="text-center pb-4" >Digite seu email para recuperar sua senha</p>
 			<form action="" method="post">
 				<label>
 					<input type="text" value="" placeholder="Inserir email">
-				</label>				
-				</label>							
+				</label>
+				</label>
 				<input type="submit" value="Enviar" class="btn">
 			</form>
 		</div>
 	</div>
+	&nbsp <a href="" class="senha text-azul mt-6 d-block">Recuperar</a>
 	</div>
 </body>
 </html>
