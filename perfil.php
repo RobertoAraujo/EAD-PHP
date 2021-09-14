744e10a0936b969f7d86f67c1fed2684df676646
Roberto Moraes <robertomoraes@lampp-it.com.br>
2021-09-14 16:39:56 -0500

    lsiatgem em andamento

--- perfil.php
+++ perfil.php
@@ -19,8 +19,8 @@
 <body>
 	<div class="base-topo">
 		<div class="conteudo">
-			<a href="" class="menu-m">menu mobile esquerdo</a><!-- aqui fico icone reponsavel pelo meno da esquerda-->
-			<a href="" class="menu-grade">menu mobile direito</a><!--aqui fica o menu responsavel pelo meno do topo-->
+			<a href="" class="menu-m">menu mobile esquerdo</a> <!-- aqui fico icone reponsavel pelo meno da esquerda--> 
+			<a href="" class="menu-grade">menu mobile direito</a> <!--aqui fica o menu responsavel pelo meno do topo--> 
 			<a href="" class="logo"></a>
 			<div id="grade">
 			<ul class="menu-topo">
